4833060cd3eead84d0835d632784b1f5a84a2830
usercreator005 <r07706048@gmail.com>
2026-01-15 15:02:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
+const authRoutes = require("./routes/auth.routes");
 /* ===============================
    ROUTES IMPORT
 ================================ */
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/", authRoutes);
 
 /* ===============================
    STATIC FOLDERS
