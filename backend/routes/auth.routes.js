d10cd31ea545769580a736d12820ad29b796ecaa
usercreator005 <r07706048@gmail.com>
2026-01-15 14:58:50 +0530

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const router = express.Router();
 
+const { adminLogin } = require("../controllers/auth.controller");
+
+router.post("/admin/login", adminLogin);
+
+module.exports = router;
