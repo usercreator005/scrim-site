c4473d7421ac3e4b01732ce1dc75328114026d2a
usercreator005 <r07706048@gmail.com>
2026-01-18 00:38:58 +0530

    Update LobbyLimit.js

--- backend/models/LobbyLimit.js
+++ backend/models/LobbyLimit.js
@@ -4,6 +4,6 @@ const lobbyLimitSchema = new mongoose.Schema({
   time: { type: String, required: true },
   fee: { type: Number, required: true },
   maxLobby: { type: Number, required: true }
-}, { timestamps: true });
+});
 
 module.exports = mongoose.model("LobbyLimit", lobbyLimitSchema);
