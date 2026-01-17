f3155805384fba8006b2e6e31a5c719871af0553
usercreator005 <r07706048@gmail.com>
2026-01-18 00:12:04 +0530

    Update Registration.js

--- backend/models/Registration.js
+++ backend/models/Registration.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
-
+const submissionSchema = new mongoose.Schema({
+  teamName: { type: String, required: true },
+  whatsapp: { type: String, required: true },
+  time: { type: String, required: true },
+  fee: { type: Number, required: true },
+  screenshot: { type: String, required: true },
+  status: { type: String, default: "pending" },
+  lobbyLink: { type: String, default: "" } // <-- New field
+}, { timestamps: true });
 const registrationSchema = new mongoose.Schema({
   teamName: String,
   whatsapp: String,
